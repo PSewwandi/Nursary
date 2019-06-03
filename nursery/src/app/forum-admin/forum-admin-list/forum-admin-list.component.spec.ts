36e8e0db55b4c9b42228f3f48a76a6d39b86607c
Sasini VD <37654454+sasinivd@users.noreply.github.com>
2019-06-03 13:25:17 +0530

    update in spec

--- nursery/src/app/forum-admin/forum-admin-list/forum-admin-list.component.spec.ts
+++ nursery/src/app/forum-admin/forum-admin-list/forum-admin-list.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DataTableDirective } from 'angular-datatables';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { ForumAdminListComponent } from './forum-admin-list.component';
-//import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 import { PostService } from 'src/app/shared/post.service';
 
@@ -11,19 +11,19 @@ fdescribe('ForumAdminListComponent', () => {
   let component: ForumAdminListComponent;
   let fixture: ComponentFixture<ForumAdminListComponent>;
   
-  /*const FirestoreStub = {
+  const FirestoreStub = {
     collection: (name: string) => ({
       doc: (_id: string) => ({
         valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
         set: (_d: any) => new Promise((resolve, _reject) => resolve()),
       }),
     }),
-  };*/
+  };
   
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ForumAdminListComponent,DataTableDirective ],
-      //providers:[{provide:AngularFirestore,useValue: FirestoreStub},{provide:AngularFireStorage,useValue:FirestoreStub},{provide:ToastrService,useValue:FirestoreStub},{provide:PostService,useValue:sortServiceSpy} ],
+      providers:[{provide:AngularFirestore,useValue: FirestoreStub},{provide:ToastrService,useValue:FirestoreStub} ],
       
     })
     .compileComponents();
